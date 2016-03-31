2176d131aca227ce856909c8dce2b7eb417787f6
Gaspard Merten <gaspardmerten-gmp@outlook.com>
2016-03-31 17:36:03 +0200

    Update

--- base.py
+++ base.py
@@ -4,7 +4,13 @@
 ### Create Your Own Minecraft Server ###
 ########################################
 from commun import *
-import requests
+
+#Requests
+import subprocess
+import os
+
+
+clear()
 yn = input("Do you really want to create your Minecraft Server ? Y/N     :")
 
 
@@ -22,37 +28,50 @@ while cont:
 
 
 #Create - Find link
-serv=input("Please enter the version of your futur server (1.x or 1.x.x) :")
-chemin=input("Now enter the folder of your server ( ex: /home/mcm/serv/ or C:/Users/mcm/serv")
-serv1 = float(serv)
-versionwant=server(serv)
-if serv1 < 1.4:
-        print("Sorry but the versions under 1.4 are not in charge by MCSM")
-elif serv1 >= 1.4:
-	ydown=input("The program go download the server jar files . Do you want to continue ? Y/N :")
-	cont = 1
-	while cont:
-		if ydown == "Y" or ydown =="y":
-			pass
-			cont = 0
-		elif ydown == "N" or ydown == "n":
-			exit(0)
-		else:
-			print("Wait one second...")
-			wait(1)
-			print("Please enter a correct value:")
-			ydown = input("The program go download the server jar files . Do you want to continue ? Y/N :")
+ServerName = input("Please enter the name of your futur server : ")
+BoucleServer=True
 
-print("We are downloading the server files, Please wait a few seconds .")
-fileName = chemin +'serv.jar'
-req = requests.get(versionwant)
-file = open(fileName, 'wb')
-for chunk in req.iter_content(100000):
-    file.write(chunk)
-file.close()
-print("The download is finish !")
+while BoucleServer:
+	ServerBase=input("Please choos :\n (1) CraftBukkit \n (2) Vanilla \n (3) Spigot\n\n Enter 1 or 2 or 3 : ")
+	if ServerBase == "1":
+		clear()
+		BoucleServer=False	
+
+	elif ServerBase == "2":
+		clear()
+		BoucleServer=False
+		versionwant1=Ch_Vanilla()
+		clear()
+		print("We are downloading the server files, Please wait a few seconds .")
+
+		download(versionwant1 , ServerName+".jar")
+		subprocess.run(['java', '-jar', 'serveur/' + ServerName + '.jar'])
+
+	elif ServerBase == "3":
+		clear()
+		BoucleServer=False
+
+	else:
+		BoucleServer = True
+		clear()
+		print("Please enter a correct value !!")
+	
+
+
+
+
+
+
+
+
+
+
+
 wait(2)
 clear()
-servstart =input("Now you need to start the server, do you want to start the server ? Y/N :")
+print("Now the program go start the server, press Ctrl + c for interupt the program !!")
+wait(2)
+
+
 
 ############################
--- commun.py
+++ commun.py
@@ -1,4 +1,6 @@
 import time
+import requests
+
 def log(logs):
 	print("[LOG]:  ", logs)
 	logfil = open("log0.log", "a")
@@ -12,11 +14,15 @@ def wait(sec):
 	time.sleep(sec) 
 def server(version):
 	a = version
-	b = a[0] + "." + a[2]
-	print(a)
+	b = a[0] + "." + a[2] 
+	if a[3]:
+		b = a[0] + "." + a[2] + "." + a[3]
 	a = str(b) + ''
+	
 	version1 = "https://s3.amazonaws.com/Minecraft.Download/versions/"+a+"/minecraft_server."+a+".jar"
-	print(version1)
+	print("\n\nYour Version: " + a)
+	print("Link of your version :" + version1 + "\n" *4)
+
 	return version1
 
 def load(sec):
@@ -51,3 +57,28 @@ def load(sec):
 		print("..")
 		wait(0.125)
 	clear()
+
+def download(url, fichier):
+	pass
+	fileName = 'serveur/' + fichier
+	req = requests.get(url)
+	file = open(fileName, 'wb')
+	for chunk in req.iter_content(100000):
+	    file.write(chunk)
+	file.close()
+	print("The download is finish !")
+
+def Ch_Vanilla():
+		serv=input("Please enter the version of your futur server (1.x or 1.x.x) :")
+		if serv[4]:
+			serv= serv[0] + serv[1] + serv[2] +serv[4]
+		else:
+			pass
+		serv1 = float(serv)
+		if serv1 < 1.4:
+		        print("Sorry but the versions under 1.4 are not in charge by MCSM")
+		        exit(0)
+
+		elif serv1 >= 1.4:
+			versionwant=server(serv)
+		return versionwant
