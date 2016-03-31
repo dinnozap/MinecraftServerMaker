0b4fdb98a5ffcfc3624bd0061e0c97b1a6f6f610
Gaspard Merten <gaspardmerten-gmp@outlook.com>
2016-03-31 21:16:27 +0200

    Download all the files
    Ce fichier permet de télécharger tout les fichiers de https://github.com/dinnozap/MinecraftServerMaker/ et les extraits et lance main.py

--- install.py
+++ install.py
@@ -1,12 +1,36 @@
+##-------- Base Function -------------
+def clear():
+	print("\n" * 100)
+clear()
+##----------------- Librairies  
 import os, sys, requests, zipfile, shutil
+from threading import Thread
+##------------------ Interaction -----------
+
+continu = True
+iwant = input("The program \'Install.py\' go download all the files, do you want to continue ? Y/N : ")
+while continu:
+	if iwant == "Y" or iwant == "y":
+		continu = False
+		clear()
+	elif iwant == "N" or iwant == "n":
+		continu = False
+		clear()
+		exit(0)
+	else:
+		clear()
+		iwant = input("Enter a correct value \n The program \'Install.py\' go download all the files, do you want to continue ? Y/N : ")
 
 
-if not os.path.exists("preserver"):
-	os.mkdir("preserver")
+##-------------Create the tmp folder --------
+if not os.path.exists("tmp"):
+	os.mkdir("tmp")
+
+##------------- Function ---------
 
 def download(url, fichier):
 	print("Start of the download !!")
-	fileName = 'preserver/' + fichier
+	fileName = 'tmp/' + fichier
 	req = requests.get(url)
 	file = open(fileName, 'wb')
 	for chunk in req.iter_content(100000):
@@ -18,17 +42,26 @@ def unzip(source , destination):
 	with zipfile.ZipFile(source) as zf:
 		zf.extractall(destination)
 
+
+
+##------------ Path Install.py ------
+
 pathname = os.path.dirname(sys.argv[0])        
 pathact = os.path.abspath(pathname) + "\\"
 print( pathact)
 
+##---------------- Download + Unzip + Copy ----------
 
-download("https://github.com/dinnozap/MinecraftServerMaker/archive/master.zip", "MinecraftServerMaker.zip")
-unzip('preserver/MinecraftServerMaker.zip', '')
+Thread(download("https://github.com/dinnozap/MinecraftServerMaker/archive/master.zip", "MinecraftServerMaker.zip")).start()
+print("Téléchargement du fichier..")
+unzip('tmp/MinecraftServerMaker.zip', '')
 
 src = "MinecraftServerMaker-master/"
 for fic in os.listdir(src):
 	shutil.copy2(os.path.join(src, fic),pathact)
-shutil.rmtree("preserver")
+
+#------------- Delete TMP, Unzip Archive, Start Main
+
+shutil.rmtree("tmp")
 shutil.rmtree("MinecraftServerMaker-master")
 import main.py
