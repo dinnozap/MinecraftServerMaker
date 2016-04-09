e54d81729c816f27e00801ce6b791d009d3cb39f
Gaspard Merten <gaspardmerten-gmp@outlook.com>
2016-04-09 22:09:38 +0200

    Update commun.py

--- commun.py
+++ commun.py
@@ -18,11 +18,12 @@ def wait(sec):
 	
 def server(version):
 	a = version
+	print(a)
 	b = a[0] +"." + a[2] 
 	if len(a) >  3:
-		b = a[0] + "." + a[2] + "." + a[4]
+		b = a[0] + "." + a[2] +"."+ a[3]
 	a = str(b) + ''
-	
+	print(a)	
 	version1 = "https://s3.amazonaws.com/Minecraft.Download/versions/"+a+"/minecraft_server."+a+".jar"
 	print("\n\nYour Version: " + a)
 	print("Link of your version :" + version1 + "\n" *4)
@@ -74,7 +75,8 @@ def download(url, fichier):
 
 def Ch_Vanilla():
 		serv=input("Please enter the version of your futur server (1.x or 1.x.x) :")
-		if len(serv) > 5:
+		if len(serv) > 3:
+			print(serv)
 			serv= serv[0] + serv[1] + serv[2] +serv[4]
 		else:
 			pass
@@ -85,12 +87,12 @@ def Ch_Vanilla():
 
 		elif serv1 >= 1.4:
 			versionwant=server(serv)
-		return versionwant
+		return versionwant, serv
 
 
 a1 = "http://getspigot.org/jenkins/job/Spigot/46/artifact/spigot-1.9.jar"
 b2= "http://getspigot.org/jenkins/job/Spigot/2/artifact/spigot-1.8.jar"
-c3 =" http://getspigot.org/spigot/spigot-1.7.10-R0.1-SNAPSHOTBuild1646.jar"
+c3 ="http://getspigot.org/spigot/spigot-1.7.10-R0.1-SNAPSHOTBuild1646.jar"
 def Ch_Spigot():
 	serv1=input("Please enter the version of your futur server (1.9 or 1.8 or 1.7.10) :")
 	if serv1 == "1.9":
@@ -99,4 +101,4 @@ def Ch_Spigot():
 		versionwant1 = b2
 	elif serv1 == "1.7.10":
 		versionwant1 = c3
-	return versionwant1
+	return versionwant1, serv1
