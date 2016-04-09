5e5ce683f1072052de8915b0134e6a8ecb34f9e2
Gaspard Merten <gaspardmerten-gmp@outlook.com>
2016-04-10 00:02:46 +0200

    Small Update
    Now the program delete the \n in the files name.info

--- launch.py
+++ launch.py
@@ -1,6 +1,14 @@
 import subprocess
-from name import *
+
+nameinfo = open("name.info", "r")
+
+ServerName = nameinfo.readline().rstrip()
+Version = nameinfo.readline().rstrip()
+VersionServer = nameinfo.readline().rstrip()
+nameinfo.close()
+
 subprocess.call(['java', '-jar', ServerName +'.jar'])
+
 
 fichier = open("eula.txt", "w")
 fichier.write("eula = true")
