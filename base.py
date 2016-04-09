abe3c9b543b33312eb21f238d163d35d9c01143b
Gaspard Merten <gaspardmerten-gmp@outlook.com>
2016-04-10 00:01:55 +0200

    Major Update
    Update: All the code has been revised by BlackWater-G,
    Now you can install your server in any directory

--- base.py
+++ base.py
@@ -11,87 +11,175 @@ import os, sys
 import threading
 
 clear()
-yn = input("Do you really want to create your Minecraft Server ? Y/N     :")
+
+def main():
+	yn = input('Do you really want to create your Minecraft Server ? Y/N     :')
 
 
-cont = 1
-while cont:
-	if yn == "Y" or yn == "y" or yn == y or yn == Y:
-		cont = 0
-	elif yn == "N" or yn == "n" or yn == N or yn == n:
-		exit(0)
-	else:
-		print("Wait one second...")
-		wait(1)
-		print("Please enter a correct value:")
-		yn = input("Do you really want to create your Minecraft Server ? Y/N")
+	cont = 1
+	while cont:
+		if yn == 'Y' or yn == 'y':
+			cont = 0
+		elif yn == 'n' or yn == 'N':
+			exit(0)
+		else:
+			print('Wait one second...')
+			wait(1)
+			print('Please enter a correct value:')
+			yn = input('Do you really want to create your Minecraft Server ? Y/N : ')
 
 
-#Create - Find ldink
-ServerName = input("Please enter the name of your futur server : ")
-if not os.path.exists(ServerName):
-	os.mkdir(ServerName)
-BoucleServer=True
+	#Create - Find ldink
+	ServerName = input('Please enter the name of your futur server : ')
 
-while BoucleServer:
-	ServerBase=input("Please choose :\n (1)Spigot \n (2) Vanilla \n\n Enter 1 or 2  : ")
-	if ServerBase == "1":
-		clear()
-		BoucleServer=False
-		versionwant2=Ch_Spigot()
-		clear()
-		print
-		print("We are downloading the server files, Please wait a few seconds .")
- 
-		download(versionwant2[0] , ServerName + "/" + ServerName+".jar")
-		download("http://164.132.53.179/msm/launch.py",ServerName + "/" + "launch.py")
-		pathname = os.path.dirname(sys.argv[0])        
-		pathact = os.path.abspath(pathname) + "\\"
-		pathact = pathact + ServerName + '/'
-		fichier = open(ServerName + "/" + "name.info", "w")
-		fichier.write(ServerName + "\n" + ServerBase + "\n"+ versionwant2[1])
-		fichier.close()
+	pathname = os.path.dirname(sys.argv[0])       
+	pathact = os.path.abspath(pathname) + '/'
+	pathact = pathact + ServerName + '/'
+	clear()
+	pathserver = input('\n\nDo you want to create your server in this path :'+ pathact +' \nY/N : ')
 
-		print("To start the server the server, launch the launch.py file in the folder of your server ( " + ServerName + ")")
+	contpathserv = True
+	pathtrue = True
 
-
-
-	elif ServerBase == "2":
-		clear()
-		BoucleServer=False
-		versionwant1=Ch_Vanilla()
-		clear()
-		print("We are downloading the server files, Please wait a few seconds .")
-		download("http://164.132.53.179/msm/launch.py",ServerName + "/" + "launch.py" )
-		download(versionwant1[0] , ServerName + "/" + ServerName+".jar")
-		pathname = os.path.dirname(sys.argv[0])        
-		pathact = os.path.abspath(pathname) + "\\"
-		pathact = pathact + ServerName + '/'
-		print("pathact")
-		fichier = open(ServerName + "/" + "name.info", "w")
-		fichier.write(ServerName +"\n"+ ServerBase +"\n"+ versionwant1[1])
-		fichier.close()
-		print("To start the server the server, launch the zero.py file in the folder " + ServerName)
-	else:
+	clear()
+	def pathcourrant():
 		BoucleServer = True
-		clear()
-		print("Please enter a correct value !!")
-	
+		while BoucleServer:
+			ServerBase=input('Please choose :\n (1) Spigot \n (2) Vanilla \n\n Enter 1 or 2  : ')
+			if ServerBase == '1':
+				clear()
+				BoucleServer=False
+				versionwant2=Ch_Spigot()
+				clear()
+				print
+				print('We are downloading the server files, Please wait a few seconds .')
+		 
+				download(versionwant2[0] , ServerName + '/' + ServerName+'.jar')
+				clear()
+				download('http://92.222.88.229/msm/launch.py',ServerName + '/' + 'launch.py')
+				fichier = open(ServerName + '/' + 'name.info', 'w')
+				fichier.write(ServerName + '\n' + ServerBase + '\n'+ versionwant2[1])
+				fichier.close()
+
+
+
+
+			elif ServerBase == '2':
+				clear()
+				BoucleServer=False
+				versionwant1=Ch_Vanilla()
+				clear()
+				print('We are downloading the server files, Please wait a few seconds .')
+				download('http://92.222.88.229/msm/launch.py',ServerName + '/' + 'launch.py' )
+				clear()
+				download(versionwant1[0] , ServerName + '/' + ServerName+'.jar')
+				print('pathact')
+				fichier = open(ServerName + '/' + 'name.info', 'w')
+				fichier.write(ServerName +'\n'+ ServerBase +'\n'+ versionwant1[1])
+				fichier.close()
+				print('To start the server the server, launch the zero.py file in the folder ' + ServerName)
+			else:
+				BoucleServer = True
+				clear()
+				print('Please enter a correct value !!')
+
+			wait(2)
+			clear()
+			print('To start the server the server, launch the launch.py file in the folder of your server :' + ServerName + "\n \n \n ")
+			print('Now the program go start the server, press Ctrl + c for interupt the program !!')
+			wait(5)
+			clear()
+			exit(0)
+			
+	def pathchoose():
+		BoucleServer = True
+		while BoucleServer:
+			ServerBase=input('Please choose :\n (1)Spigot \n (2) Vanilla \n\n Enter 1 or 2  : ')
+			if ServerBase == '1':
+				clear()
+				BoucleServer=False
+				versionwant2=Ch_Spigot()
+				clear()
+				print
+				print('We are downloading the server files, Please wait a few seconds .')
+		 
+				download(versionwant2[0] , pathact  + ServerName+'.jar')
+				download('http://92.222.88.229/msm/launch.py',pathact  + 'launch.py')
+				fichier = open(pathact + '/' + 'name.info', 'w')
+				fichier.write(ServerName + '\n' + ServerBase + '\n'+ versionwant2[1])
+				fichier.close()
+
+				print('To start the server the server, launch the launch.py file in the folder of your server ( ' + ServerName + ')')
+
+
+
+			elif ServerBase == '2':
+				clear()
+				BoucleServer=False
+				versionwant1=Ch_Vanilla()
+				clear()
+				print('We are downloading the server files, Please wait a few seconds .')
+				download('http://92.222.88.229/msm/launch.py',pathact+ '/' + 'launch.py' )
+				download(versionwant1[0] , pathact + ServerName+'.jar')
+				print('pathact')
+				fichier = open(pathact + '/' + 'name.info', 'w')
+				fichier.write(ServerName +'\n'+ ServerBase +'\n'+ versionwant1[1])
+				fichier.close()
+				print('To start the server the server, launch the zero.py file in the folder ' + ServerName)
+			else:
+				BoucleServer = True
+				clear()
+				print('Please enter a correct value !!')
+			
+
+			wait(2)
+			clear()
+			print('Now the program go start the server, press Ctrl + c for interupt the program !!')
+			wait(2)
+			clear()
+			exit(0)
+
+
+
+	while  contpathserv:
+		if pathserver == 'y' or pathserver == 'Y':
+			contpathserv = True
+			pathtrue = True
+			if not os.path.exists(ServerName):
+				os.mkdir(ServerName)
+
+			pathcourrant()
+
+		elif pathserver == 'N' or pathserver == 'n':
+			clear()
+			pathact = input("Please enter the path of your server \n  Example of path: \n    For Linux: /home/yourself/Documents \n    For Windows: C:/Users/yourself/Documents \n \n but please do not enter the name of your server,\n he go be create by this program \n Your Path:   ")
+			while pathtrue:
+				print("see")
+				if os.path.exists(pathact)==False:
+					clear()
+					pathact = input("Sorry but you enter a false path \n Please try agaon  \n Your Path:   ")
+				elif os.path.exists(pathact)==True:
+					print("Your Server gona be create in : " + pathact + "/" + ServerName)
+					contpathserv = False
+					pathtrue = False
+
+					if not os.path.exists(pathact + "/" + ServerName):
+						os.mkdir(pathact + "/" + ServerName)
+						BoucleServer=True
+					pathact = pathact + '/' + ServerName  + '/'
+					pathchoose()
+
+
+
+
+if __name__ == '__main__':
+	main()
 
 
 
 
 
 
-
-
-
-
-
-wait(2)
-clear()
-print("Now the program go start the server, press Ctrl + c for interupt the program !!")
-wait(2)
 
 
 
