0ac2a57f8af648517b6e9f89044520a0cbfa6dcf
Gaspard Merten <gaspardmerten-gmp@outlook.com>
2016-04-10 17:10:29 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,7 +1,6 @@
-import subprocess
-import os
-import zipfile
-import requests
+import subprocess, os, zipfile, requests
+
+## Function Download
 def download(url, fichier):
  pass
  fileName = fichier
@@ -11,38 +10,38 @@ def download(url, fichier):
      file.write(chunk)
  file.close()
  print("The download is finish !")
-
+## Function Unzip
 def unzip(source , destination):
  with zipfile.ZipFile(source) as zf:
   zf.extractall(destination)
 
 nameinfo = open("name.info", "r")
 
-
 ServerName = nameinfo.readline().rstrip()
 Version = nameinfo.readline().rstrip()
 VersionServer = nameinfo.readline().rstrip()
+
 nameinfo.close()
-print(VersionServer)
+
 subprocess.call(['java', '-jar', ServerName +'.jar'])
 
 
 fichier = open("eula.txt", "w")
 fichier.write("eula = true")
 fichier.close()
-
-print("Whitch type of Minecraft server you want to create ?")
-a=input("[1] Pre-Build Spigot Server [2]  Blanc Spigot Server: ")
-if a == '1':
- print(VersionServer)
- if VersionServer == '1.9':
-  print(VersionServer)
-  download('http://puu.sh/ocXv2/ef4b3abc59.zip', 'world.zip')
-  unzip('world.zip', '')
-  if not os.path.exists("plugins"):
-          os.mkdir("plugins")
-  download('https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastSuccessfulBuild/artifact/Essentials/target/Essentials-2.x-SNAPSHOT.jar', 'plugins\essentials.jar')
-  download('https://www.spigotmc.org/resources/sexymotd.2474/download?version=73466', 'plugins\motd.jar')
-  subprocess.call(['java', '-jar', ServerName +'.jar'])
-elif a==2:
- subprocess.call(['java', '-jar', ServerName +'.jar'])
+if not os.path.exists("world"):
+	print("Whitch type of Minecraft server you want to create ?")
+	a=input("[1] Pre-Build Spigot Server [2]  Blanc Spigot Server: ")
+	if a == '1':
+	 print(VersionServer)
+	 if VersionServer == '1.9':
+	  print(VersionServer)
+	  download('http://puu.sh/ocXv2/ef4b3abc59.zip', 'world.zip')
+	  unzip('world.zip', '')
+	  if not os.path.exists("plugins"):
+	          os.mkdir("plugins")
+	  download('https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastSuccessfulBuild/artifact/Essentials/target/Essentials-2.x-SNAPSHOT.jar', 'plugins\essentials.jar')
+	  download('https://www.spigotmc.org/resources/sexymotd.2474/download?version=73466', 'plugins\motd.jar')
+	  subprocess.call(['java', '-jar', ServerName +'.jar'])
+	elif a==2:
+	 subprocess.call(['java', '-jar', ServerName +'.jar'])
