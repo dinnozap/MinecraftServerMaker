66e7d327c7a6282b9edce383bf4b404192a30921
dinnozap <dinnozap@gmail.com>
2016-04-10 14:19:24 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,12 +1,29 @@
 import subprocess
+import os
+import zipfile
+import requests
+def download(url, fichier):
+ pass
+ fileName = fichier
+ req = requests.get(url)
+ file = open(fileName, 'wb')
+ for chunk in req.iter_content(100000):
+     file.write(chunk)
+ file.close()
+ print("The download is finish !")
+
+def unzip(source , destination):
+ with zipfile.ZipFile(source) as zf:
+  zf.extractall(destination)
 
 nameinfo = open("name.info", "r")
+
 
 ServerName = nameinfo.readline().rstrip()
 Version = nameinfo.readline().rstrip()
 VersionServer = nameinfo.readline().rstrip()
 nameinfo.close()
-
+print(VersionServer)
 subprocess.call(['java', '-jar', ServerName +'.jar'])
 
 
@@ -14,4 +31,18 @@ fichier = open("eula.txt", "w")
 fichier.write("eula = true")
 fichier.close()
 
-subprocess.call(['java', '-jar', ServerName +'.jar'])
+print("Whitch type of Minecraft server you want to create ?")
+a=input("[1] Pre-Build Spigot Server [2]  Blanc Spigot Server: ")
+if a == '1':
+ print(VersionServer)
+ if VersionServer == '1.9':
+  print(VersionServer)
+  download('http://puu.sh/ocXv2/ef4b3abc59.zip', 'World.zip')
+  unzip('World.zip', '')
+  if not os.path.exists("plugins"):
+          os.mkdir("plugins")
+  download('https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastSuccessfulBuild/artifact/Essentials/target/Essentials-2.x-SNAPSHOT.jar', 'plugins\essentials.jar')
+  download('https://www.spigotmc.org/resources/sexymotd.2474/download?version=73466', 'plugins\motd.jar')
+  subprocess.call(['java', '-jar', ServerName +'.jar'])
+elif a==2:
+ subprocess.call(['java', '-jar', ServerName +'.jar'])
