5675aff7d8b32af3236745daaec38df96d6ed471
Gaspard Merten <gaspardmerten-gmp@outlook.com>
2016-04-10 17:57:06 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -31,17 +31,19 @@ fichier.write("eula = true")
 fichier.close()
 if not os.path.exists("world"):
 	print("Whitch type of Minecraft server you want to create ?")
-	a=input("[1] Pre-Build Spigot Server [2]  Blanc Spigot Server: ")
+	a=input("[1] Pre-Build (Map and Plugin) Spigot Server [2]  Blanc Spigot Server [3] Sem-Build (Plugin prebuild) : ")
 	if a == '1':
 	 print(VersionServer)
-	 if VersionServer == '1.9':
-	  print(VersionServer)
+	 if VersionServer == '1.9' or VersionServer == '1.8' or VersionServer == '1.7.10':
 	  download('http://puu.sh/ocXv2/ef4b3abc59.zip', 'world.zip')
 	  unzip('world.zip', '')
 	  if not os.path.exists("plugins"):
 	          os.mkdir("plugins")
-	  download('https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastSuccessfulBuild/artifact/Essentials/target/Essentials-2.x-SNAPSHOT.jar', 'plugins\essentials.jar')
-	  download('https://www.spigotmc.org/resources/sexymotd.2474/download?version=73466', 'plugins\motd.jar')
+	  download('https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastSuccessfulBuild/artifact/Essentials/target/Essentials-2.x-SNAPSHOT.jar', 'plugins/essentials.jar')
+	  download('https://www.spigotmc.org/resources/sexymotd.2474/download?version=73466', 'plugins/motd.jar')
 	  subprocess.call(['java', '-jar', ServerName +'.jar'])
 	elif a==2:
 	 subprocess.call(['java', '-jar', ServerName +'.jar'])
+	elif a==3:
+		download('https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastSuccessfulBuild/artifact/Essentials/target/Essentials-2.x-SNAPSHOT.jar', 'plugins/essentials.jar')
+		download('https://www.spigotmc.org/resources/sexymotd.2474/download?version=73466', 'plugins/motd.jar') subprocess.call(['java', '-jar', ServerName +'.jar'])
